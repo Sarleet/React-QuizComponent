cf31e29210c690f988ca5b8c34e0de0a41db3416
Danny Sarlee <danny.sarlee@osudio.com>
2018-07-03 17:16:35 +0200

    module completed

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,6 +9,13 @@ class Quiz extends Component {
     super(props)
     this.state = { quiz_position: 1 }
   }
+
+  showNextQuestion () {
+    this.setState((state) => {
+      return { quiz_position: state.quiz_position + 1 }
+    })
+  }
+
   render () {
     const isQuizEnd = (this.state.quiz_position - 1 === quizData.quiz_questions.length)
     return (
@@ -16,11 +23,28 @@ class Quiz extends Component {
         {isQuizEnd ? (
           <QuizEnd />
         ) : (
-          <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+          <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
+            showNextQuestionHandler={this.showNextQuestion.bind(this)} />
         )}
       </div>
     )
   }
+  // alternative way from the snippet above, splitting up function(with logic)
+  //
+  // renderQuiz () {
+  //   const isQuizEnd = (this.state.quiz_position - 1 === quizData.quiz_questions.length)
+  //   return isQuizEnd ? (
+  //     <QuizEnd />
+  //   ) : (
+  //     <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+  //   )
+  // }
+  //
+  // render () {
+  //   return (
+  //     <div>{this.renderQuiz()}</div>
+  //   )
+  // }
 }
 
 export default Quiz
